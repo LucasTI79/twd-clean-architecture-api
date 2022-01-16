42cfdeb778d0e2d0f290148343d094a920d9ddeb
Lucas Feitosa Alves <lukasalves271@gmail.com>
2022-01-16 00:59:40 -0300

    Revert "feat: user repository interface"
    This reverts commit d496476e5306ad55b1e3025c46ef8a07b3df2d99.

--- src/useCases/ports/user-repository.ts
+++
@@ -1,8 +0,0 @@
-import { UserData } from '../register-user-on-mailing-list/user-data'
-
-export interface UserRepository {
-  add(user: UserData): Promise<void>,
-  findUserByEmail(email: string): Promise<UserData>,
-  findAllUsers(): Promise<UserData[]>,
-  exists(user: UserData): Promise<boolean>
-}
